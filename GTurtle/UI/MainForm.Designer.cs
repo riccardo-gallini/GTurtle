8a6864ba2063d256b15edbcfe8659325ecbca032
riccardo.gallini@gmail.com <riccardo.gallini@gmail.com>
2016-11-18 15:22:36 +0100

    implemented various shortcuts: F5 (play) - F10 (step over) - F11 (step into) - F9 (toggle breakpoint) - Ctrl+S (save)

--- GTurtle/UI/MainForm.Designer.cs
+++ GTurtle/UI/MainForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace GTurtle
+﻿using System;
+using System.Windows.Forms;
+
+namespace GTurtle
 {
     partial class MainForm
     {
@@ -37,6 +40,7 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.btnPlay = new System.Windows.Forms.ToolStripButton();
             this.btnStop = new System.Windows.Forms.ToolStripButton();
+            this.btnPause = new System.Windows.Forms.ToolStripButton();
             this.btnStepInto = new System.Windows.Forms.ToolStripButton();
             this.btnStepOver = new System.Windows.Forms.ToolStripButton();
             this.btnStepOut = new System.Windows.Forms.ToolStripButton();
@@ -44,7 +48,6 @@
             this.cmbSurfaceSize = new System.Windows.Forms.ToolStripComboBox();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.stLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.btnPause = new System.Windows.Forms.ToolStripButton();
             this.toolStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
@@ -54,8 +57,9 @@
             this.dockPanel.ActiveAutoHideContent = null;
             this.dockPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dockPanel.Location = new System.Drawing.Point(0, 39);
+            this.dockPanel.Margin = new System.Windows.Forms.Padding(2);
             this.dockPanel.Name = "dockPanel";
-            this.dockPanel.Size = new System.Drawing.Size(1006, 422);
+            this.dockPanel.Size = new System.Drawing.Size(754, 334);
             this.dockPanel.TabIndex = 3;
             // 
             // toolStrip
@@ -76,7 +80,7 @@
             this.cmbSurfaceSize});
             this.toolStrip.Location = new System.Drawing.Point(0, 0);
             this.toolStrip.Name = "toolStrip";
-            this.toolStrip.Size = new System.Drawing.Size(1006, 39);
+            this.toolStrip.Size = new System.Drawing.Size(754, 39);
             this.toolStrip.TabIndex = 6;
             this.toolStrip.Text = "toolStrip1";
             // 
@@ -129,7 +133,7 @@
             this.btnPlay.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.btnPlay.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(77, 36);
+            this.btnPlay.Size = new System.Drawing.Size(70, 36);
             this.btnPlay.Text = "PLAY";
             this.btnPlay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnPlay.ToolTipText = "Play";
@@ -140,17 +144,27 @@
             this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
             this.btnStop.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(67, 36);
+            this.btnStop.Size = new System.Drawing.Size(59, 36);
             this.btnStop.Text = "STOP";
             this.btnStop.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Image = ((System.Drawing.Image)(resources.GetObject("btnPause.Image")));
+            this.btnPause.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(65, 36);
+            this.btnPause.Text = "PAUSE";
+            this.btnPause.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
             // btnStepInto
             // 
             this.btnStepInto.Image = ((System.Drawing.Image)(resources.GetObject("btnStepInto.Image")));
             this.btnStepInto.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnStepInto.Name = "btnStepInto";
-            this.btnStepInto.Size = new System.Drawing.Size(102, 36);
+            this.btnStepInto.Size = new System.Drawing.Size(87, 36);
             this.btnStepInto.Text = "STEP INTO";
             this.btnStepInto.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnStepInto.Click += new System.EventHandler(this.btnStepInto_Click);
@@ -160,7 +174,7 @@
             this.btnStepOver.Image = ((System.Drawing.Image)(resources.GetObject("btnStepOver.Image")));
             this.btnStepOver.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnStepOver.Name = "btnStepOver";
-            this.btnStepOver.Size = new System.Drawing.Size(106, 36);
+            this.btnStepOver.Size = new System.Drawing.Size(89, 36);
             this.btnStepOver.Text = "STEP OVER";
             this.btnStepOver.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnStepOver.Click += new System.EventHandler(this.btnStepOver_Click);
@@ -170,7 +184,7 @@
             this.btnStepOut.Image = ((System.Drawing.Image)(resources.GetObject("btnStepOut.Image")));
             this.btnStepOut.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnStepOut.Name = "btnStepOut";
-            this.btnStepOut.Size = new System.Drawing.Size(98, 36);
+            this.btnStepOut.Size = new System.Drawing.Size(84, 36);
             this.btnStepOut.Text = "STEP OUT";
             this.btnStepOut.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnStepOut.Click += new System.EventHandler(this.btnStepOut_Click);
@@ -187,7 +201,7 @@
             this.cmbSurfaceSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cmbSurfaceSize.Name = "cmbSurfaceSize";
             this.cmbSurfaceSize.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
-            this.cmbSurfaceSize.Size = new System.Drawing.Size(121, 28);
+            this.cmbSurfaceSize.Size = new System.Drawing.Size(92, 23);
             this.cmbSurfaceSize.ToolTipText = "Surface Size";
             this.cmbSurfaceSize.SelectedIndexChanged += new System.EventHandler(this.cmbSurfaceSize_SelectedIndexChanged);
             // 
@@ -196,41 +210,34 @@
             this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.stLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 461);
+            this.statusStrip.Location = new System.Drawing.Point(0, 373);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(1006, 25);
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip.Size = new System.Drawing.Size(754, 22);
             this.statusStrip.TabIndex = 7;
             this.statusStrip.Text = "statusStrip1";
             // 
             // stLabel
             // 
             this.stLabel.Name = "stLabel";
-            this.stLabel.Size = new System.Drawing.Size(53, 20);
+            this.stLabel.Size = new System.Drawing.Size(41, 17);
             this.stLabel.Text = "Label1";
-            // 
-            // btnPause
-            // 
-            this.btnPause.Image = ((System.Drawing.Image)(resources.GetObject("btnPause.Image")));
-            this.btnPause.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(76, 36);
-            this.btnPause.Text = "PAUSE";
-            this.btnPause.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1006, 486);
+            this.ClientSize = new System.Drawing.Size(754, 395);
             this.Controls.Add(this.dockPanel);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.toolStrip);
             this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GTurtle";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.toolStrip.ResumeLayout(false);
             this.toolStrip.PerformLayout();
             this.statusStrip.ResumeLayout(false);
@@ -239,7 +246,7 @@
             this.PerformLayout();
 
         }
-
+        
         #endregion
         private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanel;
         private System.Windows.Forms.ToolStrip toolStrip;
